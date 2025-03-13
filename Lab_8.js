415b5e3baf18811e4c8078c3ef41a956317b1d64
DDaniels00 <dyani.daniels13@student.qcc.cuny.edu>
2025-03-13 14:44:54 -0400

    Update Lab_8.js

--- Lab_8.js
+++ Lab_8.js
@@ -7,7 +7,7 @@ let btnpressme = document.querySelector(".btnpressme")
 // add a click event to change the button text and background color when the button is clicked
 
 btnpressme.addEventListener("click", function(){
-  if(btnpressme.textContent = "PRESS ME"){
+  if(btnpressme.textContent === "PRESS ME"){
 btnpressme.textContent = "Button was pressed!"
 }
 })
