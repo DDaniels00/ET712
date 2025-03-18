a1ef59e94f10d384d91df7d7614f52be1bccfeae
DDaniels00 <dyani.daniels13@student.qcc.cuny.edu>
2025-03-18 14:35:59 -0400

    Update Lab_9.js

--- Lab_9.js
+++ Lab_9.js
@@ -1,1 +1,9 @@
+console.log("student's full name")
+//example 1
 
+// collect the element
+
+let btnscrollby = document.querySelector(".btnscrollby")
+btnscrollby.addEventListener("click", function(){
+window.scrollBy(100,0)
+})
