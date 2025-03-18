2427eb10fd056354b7099ec4d023fe044a5a1735
DDaniels00 <dyani.daniels13@student.qcc.cuny.edu>
2025-03-18 15:14:21 -0400

    Update Lab_9.js

--- Lab_9.js
+++ Lab_9.js
@@ -7,3 +7,26 @@ let btnscrollby = document.querySelector(".btnscrollby")
 btnscrollby.addEventListener("click", function(){
 window.scrollBy(100,0)
 })
+
+
+/**
+*example 2
+*/
+
+//collect the elements 
+
+let btnright = documenyt.querySelector(".btmright")
+let btnleft = document.querySelector(".btnleft")
+
+//function to scroll the gallery container
+function scrollgallery(Upsplash){
+let gallerycontainer = document.querySelector(".gallerycontainer")
+gallerycontainer.scrollBy(Upsplash)
+
+// add a click event to each buttons
+btnright.addEvenListener("click, function(){
+  scrollgallery(600)
+                         })
+btnleft.addEvenListener("click", function(){
+scrollgallery(-600)
+})
