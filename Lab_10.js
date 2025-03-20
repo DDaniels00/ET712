8080bfd3152d39c097139a283fd58d270a98296b
DDaniels00 <dyani.daniels13@student.qcc.cuny.edu>
2025-03-20 15:37:43 -0400

    Update Lab_10.js

--- Lab_10.js
+++ Lab_10.js
@@ -3,6 +3,8 @@ console.log("student's full name")
 const myform = document.querySelectr("#myform")
 const greeting = document.querySelector(".greeting")
 const greetuser = document.querySelector(".greetuser")
+const btnsubmit = document.querySelector(".btnsubmit")
+const errormsg = document.querySelector(".errormsg")
 
 window.addEventListener("load", function(event)){
   event.preventDefault()
@@ -11,15 +13,22 @@ window.addEventListener("load", function(event)){
 myform.addEventListener("submit", function(event){
 event.preventDefault()
 
-//collect the input value
+//collect the username
 let username = document.querySelector("#username")
 let usernamevalue = username.value
 
+//collect the password
+let password = document.querySelector("#username")
+let usernamevalue = username.value
+
+
 //remove whitespace before and after the username
-usernamevalue = username.value
+usernamevalue = username.value.trim()
+passwordvalue = paswordvalue.trim()
 
 //testing
 //console.log(usernamevalue)
+//validation of username
 
   if(usernamevalue===""){
   alert("Please enter a username")
