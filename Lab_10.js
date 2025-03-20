653cf7328c2e202306c89eaff11832ff13fe5cc1
DDaniels00 <dyani.daniels13@student.qcc.cuny.edu>
2025-03-20 14:47:53 -0400

    Update Lab_10.js

--- Lab_10.js
+++ Lab_10.js
@@ -4,6 +4,15 @@ const myform = document.querySelectr("#myform")
 const greeting = document.querySelector(".greeting")
 const greetuser = document.querySelector(".greetuser")
 
-myform.addEventListener("submit", function(){
+myform.addEventListener("submit", function(event){
+event.preventDefualt()
+
+//collect the input value
+let username = document.querySelector("#username")
+let usernamevalue = username.value
+
+//remove whitespace before and after the username
+
+
 })
 
