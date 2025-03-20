277726fbd4516794545bb12dd2c66baa65ac7bcf
DDaniels00 <dyani.daniels13@student.qcc.cuny.edu>
2025-03-20 14:45:01 -0400

    Update Lab_10.js

--- Lab_10.js
+++ Lab_10.js
@@ -1,1 +1,9 @@
+console.log("student's full name")
+// collect elements
+const myform = document.querySelectr("#myform")
+const greeting = document.querySelector(".greeting")
+const greetuser = document.querySelector(".greetuser")
+
+myform.addEventListener("submit", function(){
+})
 
