c22e80d58da478bfed6aeeb201e6c28c48b75d88
DDaniels00 <dyani.daniels13@student.qcc.cuny.edu>
2025-03-27 18:30:17 -0400

    Update Project_1.js

--- Project_1/Project_1.js
+++ Project_1/Project_1.js
@@ -1,1 +1,11 @@
-
+setInterval(showTime, 1000);
+        function showTime() {
+            let time = new Date();
+            let hour =
+                time.getHours();
+            let min =
+                time.getMinutes();
+            let sec =
+                time.getSeconds();
+            am_pm = "AM";
+        }
