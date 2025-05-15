71c4f10107971a7efdf27fda6faf16bd1f3b2b17
DDaniels00 <dyani.daniels13@student.qcc.cuny.edu>
2025-05-15 15:01:58 -0400

    Update App.js

--- Lab_15/App.js
+++ Lab_15/App.js
@@ -1,1 +1,23 @@
+import './App.css';
 
+function App() {
+    return (
+        <>
+            <h1> Slice App </h1>
+            <section className="slicewrapper">
+                <div className="numberWrapper">
+                <div><p>1</p></div>
+                <div><p>2</p></div>
+                <div><p>3</p></div>
+                </div>
+                <p className="msg">Press the button to slice</p>
+                <div className="btngroup">
+                <button>Previous</button>
+                <button>Next</button>
+                </div>
+            </section>
+        </>
+    );
+}
+
+export default App;
