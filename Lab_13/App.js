905a2a8a361666db72f5d437477b9b416ca46561
DDaniels00 <dyani.daniels13@student.qcc.cuny.edu>
2025-05-15 15:14:43 -0400

    Create App.js

---
+++ Lab_13/App.js
@@ -0,0 +1,24 @@
+/*navbars.js*/
+.title{
+  color:blueviolet;
+  text-align:center;
+  padding: 2%
+}
+.navheader{
+  background-color: aliceblue
+  display flex;
+  justify-content: center
+}
+.iconlink{
+  width: 30%;
+}
+.navlinks{
+  width: 30%
+  padding 2%;
+  display: flex;
+  justify-content: end;
+  background-color: aliceblue;
+}
+.tablink:hover, .iconlink:hover{
+  color: magenta;
+}
